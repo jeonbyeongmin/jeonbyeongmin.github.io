2cc6224f14f692b3ac21d5ffa18596242806d5f6
jeonbyeongmin <qudals7613@gmail.com>
2020-07-05 19:05:58 +0900

    test post

---
+++ _pages/about.md
@@ -0,0 +1,11 @@
+---
+layout: single
+permalink: "/about/"
+author_profile: true
+---
+
+<center><img src="/assets/images/profile/123.png"></center>
+
+-------------
+
+# PROJECT
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "swift",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/swift/",
+        "teaser": null
+      }]
