8627ad3141bdf3eb2d8a2e7fd79567230d4c2034
jeonbyeongmin <qudals7613@gmail.com>
2021-01-28 00:06:38 +0900

    POST :: Machine Learning 2 - Learning & Data - Microsoft learning platform mooc

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "Machine Learning 1 - Microsoft Learning Platform MOOC",
-        "excerpt":"Microsoft Learning Platform에 게시되어 있는 MS 소속 전미정님의 동영상 강의를 기반으로 작성하였다. MLP Machine Learning 1 - 개요 인공지능은 인간을 본떠서 만든 기술을 의미하고 이 인공지능을 구현하기 위한 방법 중 하나가 바로 머신러닝이다 머신러닝은 사람처럼 데이터를 수집해서 학습하도록 하는 것이다. 머신러닝을 구현하는 알고리즘에는 로지스틱 리그레션, 결정트리, 서포트 벡터 머신 등...","categories": [],
+        "title": "Machine Learning 1 - 개요 - Microsoft Learning Platform MOOC",
+        "excerpt":"Microsoft Learning Platform에 게시되어 있는 MS 소속 전미정님의 동영상 강의를 기반으로 작성하였다. 인공지능은 인간을 본떠서 만든 기술을 의미하고 이 인공지능을 구현하기 위한 방법 중 하나가 바로 머신러닝이다. 머신러닝은 사람처럼 데이터를 수집해서 학습하도록 하는 것이다. 머신러닝을 구현하는 알고리즘에는 로지스틱 리그레션, 결정트리, 서포트 벡터 머신 등 다양한 알고리즘이 존재한다. 그 중 인공신경망을...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/machine-learning-1-microsoft-learning-platform-mooc/",
+        "teaser": null
+      },{
+        "title": "Machine Learning 2 - Learning의 의미와 Data 이해 - Microsoft Learning Platform MOOC",
+        "excerpt":"머신러닝과 딥러닝은 모두 input data를 받아서 처리한 후에 머신러닝의 경우 전통적인 머신러닝 모델을, 딥러닝의 경우 뉴럴 네트워크를 활용해서 output을 만들어낸다. 이런 전반적인 흐름은 비슷하지만 머신러닝은 Feature Engineering을 하고, 딥러닝은 Feature Extraction이 일어난다는 차이가 있다. 앞서 딥러닝이 머신러닝의 한 종류라고 했는데 이게 무슨 소리냐. Feature Engineering이라는 것은 사람이 여러개의 Feature가 있을...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/mlp-machine-learning-2-learning-data/",
         "teaser": null
       }]
